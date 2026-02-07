5794e7f45ecc5b77d723201331da9707491dcda8
hariharangrazer25-afk <hariharangrazer25@gmail.com>
2026-02-07 13:09:14 +0530

    Add files via upload

---
+++ 124lmc001.py
@@ -0,0 +1,58 @@
+'''i=int(input("enter a natural number "))
+for i in range (0,10,1):
+   print (i+1)'''
+ 
+
+
+'''i=int(input("enter a number " ))
+for i in range (1, i+1,2):
+    print(i+1)'''
+
+'''i =int(input("enter a number " ))
+for i in range (1, 10 ,2 ):
+    print (i)'''
+
+'''num=int(input("enter a number "))
+for num in range (1,11,1):
+    
+  print (num,"*3=",3*num)'''
+
+'''num =int(input("enter a number"))
+for num in range (1,11,1):
+ print (num,"*5=",5*num)'''
+
+'''num =int(input("enter a number"))
+for num in range (1,110,1):
+ print (num,"*2=",2*num)'''
+
+m=10
+i =int(input("enter a number"))
+for  n in range (1,m+1,1):
+     
+    if n%2==0 or n%3==0: 
+     print(n)
+
+
+'''m=10
+i =int(input("enter a number"))
+for  n in range (1,m+1,1):
+     
+    if n%3==0 or n%5==0 or  n%7==0: 
+     print(n)'''
+
+m=10
+i =int(input("enter a number"))
+for  n in range (1,m+1,1):
+     
+    if n%3==0 or n%5==0 or  n%7==0: 
+     print(n)
+
+
+
+
+
+
+    
+
+                                              
+         
