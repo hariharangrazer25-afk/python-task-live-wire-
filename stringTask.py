1ec02d7bfc26e0b0f55839015efa7c7557baeafb
hariharangrazer25-afk <hariharangrazer25@gmail.com>
2026-02-28 13:08:55 +0530

    Add string manipulation functions in stringTask.py
    Implement various string manipulations and checks including slicing, reversing, and palindrome detection.

---
+++ stringTask.py
@@ -0,0 +1,54 @@
+
+'''s ="livewire"
+print (s)
+print (s[:])
+print (s[0:8])
+print  (s[0:])
+print (s[:8])
+print  (s[0:4])       
+print  (s[4:8])       
+print   (s[2])
+print  (s[7])
+print  (s[-6])
+print   (s[::-1])'''
+
+'''s="livewire"
+t=""
+n=len (s)
+for i in range (n-1,-1,-1):
+    t= t+s[i]
+
+print (t)'''   
+'''s="tenet"
+t=""
+n=len(s)
+for i in range (n-1,-1,-1):
+    t=t+s[i]
+if s==t:
+    print("palindrome")
+else:
+     print ("not palindrome ")'''
+
+'''s="this is python"
+print (s.lower())
+print (s.upper())
+print (s.capitalize())
+print (s.title())
+print (s.swapcase())'''
+
+'''print("python".isalpha ())
+print ("12345".isdigit ())
+print (" " .isspace ())
+print ("python ".islower ())
+print ("python".isupper())
+print ("python language".istitle())'''
+
+ n="write, a, letter, to, college"
+upper case =0
+lower case =0
+symbol    =0
+space =0
+if i. is upper ():
+upper +=1
+elif i.islower
+
