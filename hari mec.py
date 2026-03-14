02753964a28196d6c179f001a65fab1120eb3173
hariharangrazer25-afk <hariharangrazer25@gmail.com>
2026-03-14 13:11:49 +0530

    Add files via upload

---
+++ hari mec.py
@@ -0,0 +1,77 @@
+'''a={"ID1":"mounika","id2":"hari","id3":"hari",}
+print(a)
+print(["id1"])
+print(a.get("id.1"))
+print(a.keys())
+print(a.values())
+print(a.items())
+for i in a:
+    print(i,a[i])
+a.update({"id4":"prathika"})
+print(a)
+a['id3']="prasanna"
+print(a)
+a.clear()
+print(a)
+del a
+print(a)
+details ={"siva"
+    "age":20,"course":"ds+da","adress":salem:}'''
+
+a=[1,2,3,4,5]
+print("original list:",a,)
+print(a[1])
+print(a[:])
+print(a[2:5])
+print(a[2:5])
+print(a[::-1])
+print(a[:5])
+a=[1,2,3,4,5]
+b=[2,4,6,8,10]
+c=a+b
+print(c)
+print(a*2)
+a=[1,3,4,5]
+a.append(6)
+print(a)
+a.extend([7,8])
+a.insert(88,19)
+print(a)
+a.remove(8)
+print(a)
+a.pop()
+print(a)
+a.pop(5)
+print(a)
+x=[1,2,1,2,3,1,4,5,1,3,1,1,3,1,3]
+y=[] 
+                
+'''1=[1,2,1,3,1,4,5,3,1,3]
+print(i.count(1))
+1.sort(1)
+print(1)
+print(1.index(s))
+1.reverse()
+print(1)'''
+
+t1=(1,2,3,4,5)
+t2=("apple,banana,cherry",)
+t3=(1,"hello",3,14,'true')
+print("first:",t1[0])
+print("slice:",[1,4])
+t4=(6,7,8)
+print("concat:",t1+t4)
+print("repeat:",t2*2)
+
+print("3int1:",t2*2)
+print(tuple from string:",tuple("python")
+
+
+
+
+
+
+
+
+
+
